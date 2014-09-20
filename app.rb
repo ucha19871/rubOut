f848e691e2205ecc85f3d147a5f68ee909d784b1
Ucha19871 <ucha19871@gmail.com>
2014-09-20 23:09:41 +0400

    working script

--- app.rb
+++ app.rb
@@ -1,39 +1,72 @@
+require 'rubygems'
+require 'highline/import'
 require 'cli-console'
-require 'mysql'  
-  
-#my = Mysql.new(hostname, username, password, databasename)  
-con = Mysql.new('localhost', 'admin', 'pl,okm', 'ruby')  
-rs = con.query('select * from users')  
-rs.each_hash { |h| puts h['password']}
-con.close
+require 'mysql'
+require 'digest/md5'
 
-=begin
+
+
+
+
 class ShellUI
+
     private
     extend CLI::Task
 
-    public
-
-    usage 'Usage: ls'
-    desc 'List file information about current directory'
-    def ls(params)
-        Dir.foreach(Dir.pwd) do |file|
-            puts file
-        end
+    usage 'Usage: register'
+    desc 'Register a new user into Database'
+    def rg(params)
+        con = Mysql.new("localhost", "admin", "pl,okm", "ruby")
+        reg = con.query("INSERT INTO `ruby`.`users` (`id`, `username`, `password`) VALUES (NULL, '"+params[0]+"', '"+Digest::MD5.hexdigest(params[1])+"')")
+        puts "User Registered"
     end
 
-    usage 'Usage: pwd'
-    desc 'Display current directory'
 
-    def pwd(params)
-        puts "Current directory: #{Dir.pwd}"
+    usage 'Usage: login'
+    desc 'Loggin with user creadentials'
+
+    def login(params)
+
+    	def get_username(prompt="Enter Username")
+			ask(prompt) {|q| q.echo = true}
+		end
+
+    	def get_password(prompt="Enter Password")
+			ask(prompt) {|q| q.echo = "*"}
+		end
+
+		theUserName = get_username()
+		thePassword = Digest::MD5.hexdigest(get_password())
+
+		con = Mysql.new("localhost", "admin", "pl,okm", "ruby")
+		reg = con.query("SELECT * FROM `users` as u WHERE u.username = '"+theUserName+"' AND u.password = '"+thePassword+"' ")
+		
+		
+		
+		if(reg.num_rows > 0 )
+			puts "You are now loged in!"
+		else
+			puts "Username or Uassword is incorrect!"
+		end
     end
 
-    usage 'Usage: cd <Directory>'
-    desc 'Change current directory'
 
-    def cd(params)
-        Dir.chdir(params[0]) unless params.empty?
+
+    usage 'Usage: show'
+    desc 'This functions Shows user by name'
+
+    def show(params)
+		con = Mysql.new("localhost", "admin", "pl,okm", "ruby")
+        reg = con.query("SELECT * FROM `users` as u WHERE  u.username = '"+params[0]+"'")
+        if(reg.num_rows > 0 )
+			reg.each do |row|
+				puts row
+			end
+
+		else
+			puts "There is not username like: " + params[0]
+		end
+        
     end
 end
 
@@ -41,12 +74,13 @@ io = HighLine.new
 shell = ShellUI.new
 console = CLI::Console.new(io)
 
-console.addCommand('ls', shell.method(:ls), 'List files')
-console.addCommand('pwd', shell.method(:pwd), 'Current directory')
-console.addCommand('cd', shell.method(:cd), 'Change directory')
+console.addCommand('rg', shell.method(:rg), 'Register a New User')
+console.addCommand('login', shell.method(:login), 'Loggin User') { |q| q.echo = false }
+console.addCommand('show', shell.method(:show), 'Show User By username')
+
 console.addHelpCommand('help', 'Help')
 console.addExitCommand('exit', 'Exit from program')
 console.addAlias('quit', 'exit')
+console.start("%s> ",[Dir.method(:pwd)])
 
-console.start("%s> ",[Dir.method(:pwd)])=end
-=end
+
